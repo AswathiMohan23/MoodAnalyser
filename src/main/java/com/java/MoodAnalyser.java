1079940bfe046bb8dd524871ad479dcd0192dd19
AswathiMohan23 <achu999here@gmail.com>
2022-11-16 16:48:49 +0530

    [ADDED] : added test cases to analyse aString and check the moodand  respond whether happy or sad

--- src/main/java/com/java/MoodAnalyser.java
+++ src/main/java/com/java/MoodAnalyser.java
@@ -2,9 +2,15 @@ package com.java;
 
 public class MoodAnalyser {
     public static String analyseTheMood(String mood) {
-        if(mood=="Happy")
+        if (mood == "Happy")
             return "Happy";
         else
-            return "sad";
-        }
+            return "Sad";
+    }
+    public static String checkMood(String message){
+        if(message.contains("Happy"))
+            return "Happy";
+        else
+            return "Sad";
+    }
     }
--- src/test/java/com/java/MoodAnalyserTest.java
+++ src/test/java/com/java/MoodAnalyserTest.java
@@ -8,7 +8,15 @@ import static junit.framework.TestCase.assertEquals;
 public class MoodAnalyserTest {
     @Test
     public void analyse_Mood_And_Respond(){
-        assertEquals("Sad", MoodAnalyser.analyseTheMood("Sad"));
         assertEquals("Happy", MoodAnalyser.analyseTheMood("Happy"));
+        assertEquals("Sad", MoodAnalyser.analyseTheMood("Sad"));
+
+    }
+    @Test
+    public void analyse_Mood_Return_Mood(){
+        assertEquals("Happy", MoodAnalyser.checkMood("I am Happy"));
+        assertEquals("Sad", MoodAnalyser.checkMood("I am Sad"));
+
+
     }
 }
